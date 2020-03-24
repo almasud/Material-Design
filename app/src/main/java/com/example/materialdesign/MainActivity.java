3c2efc2a736c602f94d57dbd46bc23cb342a3d2c
Almasud <almasud.arm@gmail.com>
2020-03-24 12:16:05 +0600

    A snippet to maintain compatibility of material design features in Java code

--- app/src/main/java/com/example/materialdesign/MainActivity.java
+++ app/src/main/java/com/example/materialdesign/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.materialdesign;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Check if we're running on android 5.0 (API 21) or higher
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            // Call material design APIs here
+        } else {
+            // Implement the feature without material design
+        }
     }
 }
