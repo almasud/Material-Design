a40ad72cb08cc6814d0e506e2fdc0adb13951090
Almasud <almasud.arm@gmail.com>
2020-03-25 11:19:00 +0600

    Examples links are placed in MainActivity and individual are palced to be in separate activity

--- app/src/main/java/com/example/materialdesign/MainActivity.java
+++ app/src/main/java/com/example/materialdesign/MainActivity.java
@@ -2,15 +2,21 @@ package com.example.materialdesign;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.materialdesign.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding mViewBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mViewBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mViewBinding.getRoot());
 
         // Check if we're running on android 5.0 (API 21) or higher
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
@@ -18,5 +24,7 @@ public class MainActivity extends AppCompatActivity {
         } else {
             // Implement the feature without material design
         }
+
+        mViewBinding.buttonColorPalette.setOnClickListener(view -> startActivity(new Intent(getApplicationContext(), ColorPaletteActivity.class)));
     }
 }
